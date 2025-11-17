fa728af94dc12c89bfae9ace11adc52ebc359e43
ChinCobre <1DAM@A11-PC23>
2025-11-17 12:57:52 +0100

    Merge branch 'main' of https://github.com/ChinCobre/Reto1OctavianSergioAdrian.git into main

--- Reto1SergioOctavianAdrian/src/Reto/Funciones1.java
+++ Reto1SergioOctavianAdrian/src/Reto/Funciones1.java
@@ -2,4 +2,25 @@ package Reto;
 
 public class Funciones1 {
 
+	
+	public static int maximo3(int n1, int n2, int n3) {
+		int m=Math.max(n2, n3);
+		int mayor=Math.max(n3, m);
+		return mayor;
+	}
+	
+	public static int minimo3(int n1, int n2, int n3) {
+		int m=Math.min(n2, n3);
+		int menor=Math.max(n3, m);
+		return menor;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
